5df278e1c55beda49d2f434d986aad6a9e988313
Vanderson-Oliveira12 <vanderson.doliveira14@gmail.com>
2023-11-15 15:04:15 -0300

    create carrousel and adjusting resposively

--- js/app.js
+++ js/app.js
@@ -1,18 +1,58 @@
-function onMenu () {
-    let menuEl = document.querySelector(".btn-menu")
-    let closeEl = document.querySelector(".btn-close")
-    
-    let navEl = document.querySelector(".nav-list")
+function onMenu() {
+  let menuEl = document.querySelector(".btn-menu");
+  let closeEl = document.querySelector(".btn-close");
 
-    menuEl.addEventListener("click", handleOpenMenu)
-    closeEl.addEventListener("click", handleCloseMenu)
+  let navEl = document.querySelector(".nav-list");
 
-    function handleOpenMenu () {
-        navEl.classList.add("active")
-    }
-    function handleCloseMenu () {
-        navEl.classList.remove("active")
-    }
+  menuEl.addEventListener("click", handleOpenMenu);
+  closeEl.addEventListener("click", handleCloseMenu);
+
+  function handleOpenMenu() {
+    navEl.classList.add("active");
+  }
+  function handleCloseMenu() {
+    navEl.classList.remove("active");
+  }
 }
 
+function onMenuScroll() {
+  let menuEl = document.querySelector("#nav");
+
+  window.addEventListener("scroll", calcMenuPos);
+
+  function calcMenuPos(e) {
+    let posScroll = window.scrollY;
+
+    if (posScroll > 15) {
+      menuEl.classList.add("nav-fixed");
+
+      return;
+    }
+
+    menuEl.classList.remove("nav-fixed");
+  }
+}
+
+function onCarousel() {
+  let carouselContainerEl = document.querySelector(".about-carousel-container");
+  let carouselImages = carouselContainerEl.querySelectorAll("img");
+
+  let imagesLength = carouselImages.length - 1;
+  let index = 0;
+
+  setInterval(() => {
+    carouselImages[index].classList.remove("shown");
+
+    index++;
+
+    if (index > imagesLength) {
+      index = 0;
+    }
+
+    carouselImages[index].classList.add("shown");
+  }, 3000);
+}
+
+onCarousel();
 onMenu();
+onMenuScroll();
